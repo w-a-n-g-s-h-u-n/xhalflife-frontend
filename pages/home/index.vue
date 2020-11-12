c66fda87acd07f77c7dda80ecf537380c635a453
ennea8 <ennea8@gmail.com>
2020-11-12 14:47:15 +0800

    Opt pages structure

---
+++ pages/home/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">
+        xhalflife
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import { STREAM_LIST } from '@/api/apollo/queries'
+export default {
+  data () {
+    return {
+      a: process.env.A
+    }
+  },
+  async mounted () {
+    console.log('TEST $apollo query')
+    const ret = await this.$apollo.query({ query: STREAM_LIST, variables: { first: 10 } })
+    console.log('ret', ret)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "./index.scss";
+</style>
