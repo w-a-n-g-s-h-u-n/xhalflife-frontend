276e795c20a818979e0692bb5f250305a3f88a32
ennea8 <ennea8@gmail.com>
2020-11-13 09:33:33 +0800

    Login

---
+++ components/Login.vue
@@ -0,0 +1,39 @@
+<template>
+  <el-button :disabled="isMetaMaskConnected" @click="onClick">
+    {{ isMetaMaskConnected?'MetaMask Connected':'Connect MetaMask' }}
+  </el-button>
+</template>
+
+<script>
+import metamask from '@/api/wallet/metamask'
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Login',
+  data () {
+    return {}
+  },
+  computed: {
+    ...mapGetters([
+      'isMetaMaskConnected'
+    ])
+  },
+  mounted () {
+    console.log('Login mounted')
+  },
+  methods: {
+    onClick () {
+      if (!this.isMetaMaskConnected) {
+        this.connect()
+      }
+    },
+    async connect () {
+      const accounts = await metamask.connectMetaMask()
+      this.$store.commit('updateAccounts', { accounts, source: 'MetaMask' })
+    }
+  }
+}
+</script>
+
+<!--<style scoped>-->
+<!--</style>-->
