6b208f815bdb0a72ab89f6decf8fb3c93d9ec772
ennea8 <ennea8@gmail.com>
2020-11-13 09:31:35 +0800

    api/apollo

--- api/apollo/client.js
+++ api/apollo/client.js
@@ -4,7 +4,7 @@ import { HttpLink } from 'apollo-link-http'
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://api.thegraph.com/subgraphs/name/xdefilab/xhalflifekovan'
+    uri: process.env.APOLLO_BASE_URL
   }),
   cache: new InMemoryCache(),
   shouldBatch: true
