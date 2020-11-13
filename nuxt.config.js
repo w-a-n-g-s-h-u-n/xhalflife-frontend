959cc6176f280b038d2b17fc70171df82aad354a
ennea8 <ennea8@gmail.com>
2020-11-13 09:34:49 +0800

    config/package/home/utils

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,8 @@ export default {
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
     '@/plugins/element-ui',
-    '@/plugins/apollo'
+    '@/plugins/apollo',
+    '@/plugins/app-mounted.js'
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
--- pages/home/index.vue
+++ pages/home/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
+    <Login />
     <div>
       <Logo />
       <h1 class="title">
@@ -12,6 +13,8 @@
 <script>
 
 import { STREAM_LIST } from '@/api/apollo/queries'
+import { getProvider } from '@/api/contract/ethers'
+import { XHalfLife } from '@/api/contract'
 export default {
   data () {
     return {
@@ -22,6 +25,17 @@ export default {
     console.log('TEST $apollo query')
     const ret = await this.$apollo.query({ query: STREAM_LIST, variables: { first: 10 } })
     console.log('ret', ret)
+
+    const provider = getProvider()
+    console.log('provider', provider)
+
+    const balance = await provider.getBalance('0xc3bcc607335ae9EA59736700A87C1E3bc0ec32D9')
+    console.log(balance.toString())
+
+    const v = await XHalfLife.getVersion()
+    console.log('XHalfLife getVersion', v)
+
+    // const b2 = await XHalfLifeIns.balanceOf('0xc3bcc607335ae9EA59736700A87C1E3bc0ec32D9')
   }
 }
 </script>
