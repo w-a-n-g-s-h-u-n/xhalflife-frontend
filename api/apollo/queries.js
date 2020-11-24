6dbd93c528c0dc5af5cba3bc258227c68036c6f1
ennea8 <ennea8@gmail.com>
2020-11-25 06:25:53 +0800

    STREAM_LIST query

--- api/apollo/queries.js
+++ api/apollo/queries.js
@@ -1,26 +1,27 @@
 import gql from 'graphql-tag'
 
 // TODO 参数化、分页
-export const STREAM_LIST = gql`{
-streams(first: 10) {
-    id
-    sender
-    recipient
-    depositAmount
-    startBlock
-    kBlock
-    unlockRatio
-    timestamp
-    txs(first: 10, orderBy: timestamp, orderDirection: desc) {
-      block
-      event
-      from
-      timestamp
-      to
-      txhash
-    }
+export const STREAM_LIST = gql`
+  query streams($first: Int!) {
+    streams(first: $first) {
+        id
+        sender
+        recipient
+        depositAmount
+        startBlock
+        kBlock
+        unlockRatio
+        timestamp
+        txs(first: $first, orderBy: timestamp, orderDirection: desc) {
+          block
+          event
+          from
+          timestamp
+          to
+          txhash
+        }
+      }
   }
-}
  `
 
 export const STREAM_GET_TOTAL_DATA = gql`{
