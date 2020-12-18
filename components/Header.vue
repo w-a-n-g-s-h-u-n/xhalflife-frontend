882bdec95323ad701c65f6bc1b257d05b73e4d8e
lavender78 <lindawu8134@gmail.com>
2020-12-18 16:36:08 +0800

    feat: add others website to halflife

--- components/Header.vue
+++ components/Header.vue
@@ -1,97 +1,45 @@
 <template>
-  <el-header class="header">
+  <el-header class="header" height='78px'>
     <div class="content">
-      <div class="navContent">
-        <img class="logo" src="~/assets/xdefi.svg" alt="">
+      <img class="logo" src="~/assets/xdefi.svg" alt="">
+      <div class='navContent'>
         <span class="nav-item active">
           <NuxtLink to="/"><span class="text">HalfLife</span></NuxtLink>
           <span class="indicator" />
         </span>
+        <Navs />
       </div>
-      <Login/>
+      <i class="el-icon-s-fold navIcon" @click="drawer=true"/>
+      <el-drawer
+        :visible.sync="drawer"
+        :show-close="false"
+        :wrapperClosable="true"
+        size="50%"
+        class="drawer"
+      >
+        <Navs />
+      </el-drawer>
+      <Login class="account"/>
     </div>
   </el-header>
 </template>
 
 <script>
+import Navs from './Navs'
+
 export default {
-  name: 'Header'
+  name: 'Header',
+  components: {
+    Navs
+  },
+  data () {
+    return {
+      drawer: false
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-  @import "~/pages/home/index.scss";
-
-  .header {
-    background-color: #161638;
-    color: #333;
-    text-align: center;
-    height: 60px;
-    width: 100%;
-    padding: 0;
-
-    .content {
-      width: 1000px;
-      position: relative;
-      margin: 0 auto;
-      height: 100%;
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-
-      .navContent {
-        flex: 1 1 70%;
-        display: flex;
-      }
-
-      .logo {
-        width: 110px;
-        height: 30px;
-        position: absolute;
-        left: -110px;
-      }
-
-      .nav-item.active {
-        display: flex;
-        flex-direction: column;
-        justify-content: space-around;
-        align-items: center;
-        height: 33px;
-        margin-left: 50px;
-        width: 80px;
-
-        .text {
-          /* font-family: pingfangsc-semibold; */
-          font-size: 16px;
-          color: #fff;
-          letter-spacing: 0;
-          text-align: center;
-        }
-
-        .indicator {
-          background-image: linear-gradient(136deg, #2bf7dd 0%, #3a8ff7 51%, #da37fa 100%);
-          border-radius: 3px;
-          width: 20px;
-          height: 4px;
-          margin-top: 7px;
-        }
-      }
-
-      @media (max-width: 768px) {
-        width: 100%;
-        padding: 0 0.5rem;
-
-        .logo {
-          width: auto;
-          position: relative;
-          left: 0.5rem;
-          height: 1.5rem;
-        }
-
-        .nav-item.active {
-          margin-left: 1rem;
-        }
-      }
-    }
-  }
+@import './header.scss';
 </style>
