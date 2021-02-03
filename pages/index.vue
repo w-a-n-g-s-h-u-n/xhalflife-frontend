c1e88d4014006b9db4f55049aa786235b659893c
liuyuhuan <liuyanhuan@ats.org.cn>
2021-02-03 16:31:51 +0800

    remove bg

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,5 @@
 <template>
-  <div class="mainParent">
-    <div class="hKxtfS"><img src="../assets/bg.png" /></div>
-    <Home />
-    <div class="fQDgZz"><img src="../assets/bg.png" /></div>
-  </div>
+  <Home />
 </template>
 
 <script>
@@ -12,34 +8,3 @@ export default {
   components: { Home }
 };
 </script>
-<style lang="scss" scoped>
-  .mainParent{
-    position: relative;
-    overflow: hidden;
-  }
-.hKxtfS {
-  width: 50%;
-  min-width: 300px;
-  position: absolute;
-  left: 200px;
-  top: 0px;
-  z-index: 0;
-}
-.hKxtfS img {
-  width: 100%;
-}
-.fQDgZz img {
-  width: 100%;
-}
-.fQDgZz {
-  position: absolute;
-  bottom: 0px;
-  width: 50%;
-  min-width: 300px;
-  left: auto;
-  margin-top: 0px;
-  transform: rotateX(180deg);
-  top: auto;
-  right: 100px !important;
-}
-</style>
